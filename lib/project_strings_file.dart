578feb3e4c1b8c9a2b92f94350bc3b13d46205cd
Brandon Guerin <Boomersooner12345@gmail.com>
2022-04-23 11:20:20 -0400

    Added a symptom selector list for selecting multiple symptoms without having to type it in.

---
+++ lib/project_strings_file.dart
@@ -0,0 +1,14 @@
+var symptomList = [
+'Impulsiveness','Impatience','Hyperactivity',
+'Working Memory Issues','Executive Dysfunction',
+'Mental Sluggishness/Dopamine Hunt','Having a bad day',
+'Distractions','Brain Fog','No Symptoms',
+'Positive Environment/Grace',
+'Momentum driven','Sensory Overload',
+'A win today','Unable to focus','Struggling to focus',
+'Hyperfocus','ADHD Burnout',
+'Lacking Motivation','Stress enhanced ADHD Response',
+  'Existential crisis','Freeze/Mental Paralysis',
+'Procrastination','Emotional Flooding','Emotion fueled response',
+'Emotional Meltdown', 'Rejection Sensitivity Dysphoria',
+'Depression','Anxiety' ];
---
+++ lib/symptom_selector_screen.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'compose_records_screen.dart';
+import 'project_strings_file.dart';
+
+
+
+class SymptomSelectorScreen extends StatefulWidget{
+  SymptomSelectorScreen({Key? key, required this.symptoms}) : super(key: key);
+String symptoms;
+
+@override
+  State<SymptomSelectorScreen> createState() => _SymptomSelectorScreen();
+
+
+}
+
+class _SymptomSelectorScreen extends State<SymptomSelectorScreen>{
+//String symptoms ='';
+Set<String> symptomsChecked = <String>{};
+List<Symptoms> symptomListSelection = Symptoms.getSymptoms();
+@override
+  void initState() {
+    super.initState();
+loadSymptoms();
+  }
+ void loadSymptoms(){
+  symptomsChecked.addAll(super.widget.symptoms.split(','));
+  for(Symptoms element in symptomListSelection){
+    if(symptomsChecked.contains(element.symptom)){
+    element.isChecked = true;
+    }
+  }
+ }
+
+
+  void addItemsToSymptomList(){
+  super.widget.symptoms = '';
+  for(String element in symptomsChecked)
+    {
+      super.widget.symptoms+=element + ',';
+    }
+
+
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(title: const Text('ADHD Symptom Selection'),),
+    body:
+    ListView.separated(
+itemCount: symptomList.length,
+    separatorBuilder: (BuildContext context, int index) => Divider(),
+  itemBuilder: (BuildContext context, int index)=> CheckboxListTile(
+      value: symptomListSelection[index].isChecked,
+      onChanged: (bool? changed){
+setState(() {
+  symptomListSelection[index].isChecked = changed!;
+  if(changed == true){
+    symptomsChecked.add(symptomListSelection[index].symptom);
+  }
+  else{
+    symptomsChecked.remove(symptomListSelection[index].symptom);
+  }
+  print(symptomsChecked);
+  print(symptomListSelection);
+});
+},
+      title: Text(symptomListSelection[index].symptom)),
+
+
+  ) ,
+
+    floatingActionButton: FloatingActionButton.extended(label: const Text('Save'),
+      onPressed: (){
+        addItemsToSymptomList();
+        Navigator.pop(context,super.widget.symptoms);
+      },
+    ),
+  );
+  }
+
+
+
+
+
+}
+
+class Symptoms {
+  String symptom ;
+  bool isChecked ;
+
+  Symptoms(this.symptom,this.isChecked);
+  static List<Symptoms> getSymptoms(){
+   List<Symptoms> symptomsList = List.empty(growable: true);
+    for (String element in symptomList){
+      symptomsList.add(Symptoms(element, false));
+    }
+    return symptomsList;
+  }
+  @override
+  String toString() {
+return symptom + ' $isChecked\r\n';
+  }
+}
