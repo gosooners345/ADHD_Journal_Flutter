8332d5994c6d071adfc21dbc00f124ac7e34a1f7
gosooners345 <boomersooner12345@gmail.com>
2023-03-14 13:45:18 -0400

    Created shell for OneDrive sync methods

--- lib/backup_providers/onedrive_api_integration.dart
+++ lib/backup_providers/onedrive_api_integration.dart
@@ -1,1 +1,20 @@
+import 'dart:typed_data';
+
 import 'package:flutter_onedrive/flutter_onedrive.dart';
+
+
+class OneDriveSyncClass{
+
+  final oneDriveUtility = OneDrive(clientID: '', redirectURL: '');
+
+
+  void pushFileToCloud(String fileName){
+
+    oneDriveUtility.push(Uint8List(2), fileName);
+  }
+
+  void downloadFileFromCloud(String fileName){
+    oneDriveUtility.pull(fileName);
+  }
+
+}
