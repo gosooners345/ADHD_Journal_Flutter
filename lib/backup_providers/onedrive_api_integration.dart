7882430b7af427a650d699be61f0ce9273ec9025
Brandon Guerin <Boomersooner12345@gmail.com>
2023-03-20 16:21:50 -0400

    Add comment warning about OneDrive class limitations

--- lib/backup_providers/onedrive_api_integration.dart
+++ lib/backup_providers/onedrive_api_integration.dart
@@ -2,7 +2,7 @@ import 'dart:typed_data';
 
 import 'package:flutter_onedrive/flutter_onedrive.dart';
 
-
+/// Need to expand OneDrive class to go further and force authentication when activated or else the data can easily be accessed or backups will not work properly
 class OneDriveSyncClass{
 
   final oneDriveUtility = OneDrive(clientID: 'abdfccc0-c909-4782-aab7-c6db9400c042', redirectURL: 'https://login.live.com/oauth20_desktop.srf');
