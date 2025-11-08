1d3bcc640d7db7de6c08db112decbc3d6ee5671b
Brandon Guerin <boomersooner12345@gmail.com>
2025-11-08 13:37:22 -0500

    Fixed a networking call made that triggered errors.

--- lib/ui_components/loading_card_widget.dart
+++ lib/ui_components/loading_card_widget.dart
@@ -34,11 +34,9 @@ class CardListItem extends StatelessWidget {
         ),
         child: ClipRRect(
           borderRadius: BorderRadius.circular(16),
-          child: Image.network(
-            'https://docs.flutter.dev/cookbook'
-                '/img-files/effects/split-check/Food1.jpg',
-            fit: BoxFit.cover,
-          ),
+          child: Text('Loading'),
+
+
         ),
       ),
     );
